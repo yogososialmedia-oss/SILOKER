5e59c9bec7a6afbb504c6c5c2d6aa4bb79a1e235
yogososialmedia-oss <yogososialmedia@gmail.com>
2026-02-05 17:14:55 +0800

    Nambah registrasi perusahaan

--- resources/views/view_perusahaan/edit-profile-perusahaan.blade.php
+++ resources/views/view_perusahaan/edit-profile-perusahaan.blade.php
@@ -42,7 +42,7 @@
                   <textarea name="TentangPerusahaan" id="exampleFormControlTextarea1" class="form-control" rows="3"></textarea>
                 </div>
                 <div class="col-mb text-end">
-                  <button type="button" class="btn btn-primary">Save</button>
+                  <button type="button" class="btn btn-primary">Edit</button>
                 </div>
               </div>
             </form>
--- resources/views/view_perusahaan/registrasi-perusahaan.blade.php
+++ resources/views/view_perusahaan/registrasi-perusahaan.blade.php
@@ -1,80 +1,174 @@
-<x-pencari_kerja.layout>
-    <!-- Content wrapper -->
-    <div class="content-wrapper">
-        <!-- Content -->
-        <div class="container-xxl flex-grow-1 container-p-y">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <form action="" method="POST" enctype="multipart/form-data">
+<!doctype html>
+
+<html lang="en" class="layout-wide customizer-hide" data-assets-path="../assets/"
+    data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Login Perusahaan</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('admin-perusahaan/assets/img/favicon/favicon.ico')}}" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ asset('admin-perusahaan/assets/vendor/fonts/iconify-icons.css')}}" />
+
+    <!-- Core CSS -->
+    <!-- build:css assets/vendor/css/theme.css  -->
+
+    <link rel="stylesheet" href="{{ asset('admin-perusahaan/assets/vendor/css/core.css')}}" />
+    <link rel="stylesheet" href="{{ asset('admin-perusahaan/assets/css/demo.css')}}" />
+
+    <!-- Vendors CSS -->
+
+    <link rel="stylesheet"
+        href="{{ asset('admin-perusahaan/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+
+    <!-- endbuild -->
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('admin-perusahaan/assets/vendor/css/pages/page-auth.css')}}" />
+
+    <!-- Helpers -->
+    <script src="{{ asset('admin-perusahaan/assets/vendor/js/helpers.js')}}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+
+    <script src="{{ asset('admin-perusahaan/assets/js/config.js')}}"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <div class="container-fluid">
+            <span class="app-brand-text demo menu-text fw-bold text-white me-5">
+                Career Center
+            </span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-ex-7">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbar-ex-7">
+                <div class="navbar-nav me-auto ms-4">
+                    <a class="nav-item nav-link active" href="javascript:void(0)">Home</a>
+                    <a class="nav-item nav-link" href="javascript:void(0)">About</a>
+                    <a class="nav-item nav-link" href="javascript:void(0)">Loker</a>
+                </div>
+                <div class="navbar-nav ms-lg-auto" id="navbar-ex-15">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-bs-toggle="dropdown"
+                                data-trigger="hover">Login</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="javascript:void(0)">Admin</a>
+                                <a class="dropdown-item" href="javascript:void(0)">Perusahaan</a>
+                                <a class="dropdown-item" href="javascript:void(0)">Pencari Kerja</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="javascript:void(0)"><i
+                                    class="icon-base navbar-icon bx bx-user"></i>
+                                Profile</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Content -->
+
+    <div class="container-xxl flex-grow-1 container-p-y mt-12">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="row">
-                                
-                                    <div class="col-md-6 mb-3">
-                                        <label class="form-label">Nama Perusahaan</label>
-                                        <input name="NamaPerusahaan" class="form-control" placeholder="">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <label class="form-label">No NPWP</label>
-                                        <input name="NoNpwp" class="form-control" placeholder="">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <label class="form-label">Email</label>
-                                        <input name="Email" class="form-control" placeholder="">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <label class="form-label">No.Telp</label>
-                                        <input name="NoTelp" class="form-control" placeholder="">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <label class="form-label">Provinsi</label>
-                                        <input name="Provinsi" type="text" class="form-control">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <label class="form-label">Kabupaten</label>
-                                        <input name="Kabupaten" type="text" class="form-control">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <label class="form-label">Kecamatan</label>
-                                        <input name="Kecamatan" type="text" class="form-control">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    
-                                    <div class="col-md-6 mb-3">
-                                        <label class="form-label">Logo</label>
-                                        <input name="logo" type="file" class="form-control">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <label class="form-label">Banner</label>
-                                        <input name="banner" type="file" class="form-control">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="col mb-6">
-                                        <label for="exampleFormControlTextarea1" class="form-label">Tentang Perusahaan
-                                            Perusahaan</label>
-                                        <textarea name="TentangPerusahaan"class="form-control" id="exampleFormControlTextarea1"
-                                            rows="3"></textarea>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Nama Perusahaan</label>
+                                    <input name="NamaPerusahaan" class="form-control"
+                                        placeholder="Tambahkan nama perusahaan">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">No NPWP</label>
+                                    <input name="NoNpwp" class="form-control" placeholder="Tambahkan nomor NPWP">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Email</label>
+                                    <input name="Email" class="form-control"
+                                        placeholder="Tambahkan alamat email perusahaan">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">No.Telp</label>
+                                    <input name="NoTelp" class="form-control"
+                                        placeholder="Tambahkan nomor telepon perusahaan">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Provinsi</label>
+                                    <input name="Provinsi" type="text" class="form-control">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Kabupaten</label>
+                                    <input name="Kabupaten" type="text" class="form-control">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Kecamatan</label>
+                                    <input name="Kecamatan" type="text" class="form-control">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Google Maps</label>
+                                    <input name="GoogleMaps" class="form-control"
+                                        placeholder="Tambahkan link google maps lokasi perusahaan">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Logo</label>
+                                    <input name="logo" type="file" class="form-control">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <label class="form-label">Banner</label>
+                                    <input name="banner" type="file" class="form-control">
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="col mb-6">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Tentang
+                                        Perusahaan</label>
+                                    <textarea name="TentangPerusahaan" class="form-control"
+                                        id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                                <div class="col-mb text-end">
+                                    <button type="submit" class="btn btn-primary">Registrasi</button>
+                                </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
+    </div>
 
     <!-- / Content -->
-
     <!-- Footer -->
     <footer class="content-footer footer bg-footer-theme">
         <div class="container-xxl">
@@ -87,8 +181,29 @@
         </div>
     </footer>
     <!-- / Footer -->
+    <!-- Core JS -->
 
-    <div class="content-backdrop fade"></div>
-    </div>
-    <!-- Content wrapper -->
-</x-pencari_kerja.layout>
+    <script src="{{ asset('admin-perusahaan/assets/vendor/libs/jquery/jquery.js')}}"></script>
+
+    <script src="{{ asset('admin-perusahaan/assets/vendor/libs/popper/popper.js')}}"></script>
+    <script src="{{ asset('admin-perusahaan/assets/vendor/js/bootstrap.js')}}"></script>
+
+    <script src="{{ asset('admin-perusahaan/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+
+    <script src="{{ asset('admin-perusahaan/assets/vendor/js/menu.js')}}"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+
+    <script src="{{ asset('admin-perusahaan/assets/js/main.js')}}"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag before closing body tag for github widget button. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
