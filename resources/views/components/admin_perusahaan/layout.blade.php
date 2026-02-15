5a199b2ba1b27c3d09430501988ac362d8d560a3
wahyu-pranataaa <ikadekwahyu07@gmail.com>
2026-02-15 19:46:07 +0800

    Add error alert display in admin layout

--- resources/views/components/admin_perusahaan/layout.blade.php
+++ resources/views/components/admin_perusahaan/layout.blade.php
@@ -52,6 +52,14 @@
                         </ul>
                     </div>
                 </nav>
+                <div class="container-xxl mt-5 pt-4">
+                    @if(session('error'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{ session('error') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                        </div>
+                    @endif
+                </div>
                 {{ $slot }}
             </div>
             <!-- / Layout page -->
