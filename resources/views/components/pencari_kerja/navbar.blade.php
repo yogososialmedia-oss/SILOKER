32c7d1fe4d1bf60ae81622bc357ccad3786b9525
wahyu-pranataaa <ikadekwahyu07@gmail.com>
2026-02-22 14:58:47 +0800

    button login saat sudah login pencari kerja, muncul lagi kalau sudah logout

--- resources/views/components/pencari_kerja/navbar.blade.php
+++ resources/views/components/pencari_kerja/navbar.blade.php
@@ -29,19 +29,20 @@
 
             <!-- MENU KANAN -->
             <div class="navbar-nav ms-auto align-items-lg-center menu-kanan">
-                <!-- LOGIN DROPDOWN -->
-                <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle login-link-beranda" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Login
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-beranda">
-                        <li><a class="dropdown-item" href="{{ route('admin.login') }}">Admin</a></li>
-                        <li><a class="dropdown-item" href="{{ route('perusahaan.login') }}">Perusahaan</a></li>
-                        <li><a class="dropdown-item" href="{{ route('pencarikerja.login') }}">Pencari Kerja</a></li>
-                    </ul>
-                </div>
-
+                <!-- JIKA BELUM LOGIN -->
+                @if (!Auth::guard('pencarikerja')->check())
+                    <div class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle login-link-beranda" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Login
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end dropdown-beranda">
+                            <li><a class="dropdown-item" href="{{ route('admin.login') }}">Admin</a></li>
+                            <li><a class="dropdown-item" href="{{ route('perusahaan.login') }}">Perusahaan</a></li>
+                            <li><a class="dropdown-item" href="{{ route('pencarikerja.login') }}">Pencari Kerja</a></li>
+                        </ul>
+                    </div>
+                @endif
                 <!-- PROFILE -->
                     @if ((Auth::guard('pencarikerja')->user()))
                 <a class="nav-link profile-link-beranda ms-lg-3
